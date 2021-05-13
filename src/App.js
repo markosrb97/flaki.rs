56ec989de353e446f052197c98b3d88cf03c6fde
obrad97 <janiobrad@gmail.com>
2021-05-13 17:31:49 +0200

    update

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import Login from './pages/Login';
 import AboutUs from './pages/AboutUs';
 import Contact from './pages/Contact';
 import Register from './pages/Register';
+import dog from './pages/Dog.js';
+import cat from './pages/Cat.js';
+import bird from './pages/Bird.js';
+import rabbit from './pages/Rabbit.js';
+import fish from './pages/Fish.js';
+import rodent from './pages/Rodent.js';
+import reptil from './pages/Reptil.js';
+import domesticAnimal from './pages/DomesticAnimal.js';
+import arthropod from './pages/Arthropod.js';
 /*Components */
 import Header from './components/Header';
 
@@ -21,6 +30,15 @@ function App() {
           <Route path='/o-nama' component={AboutUs}></Route>
           <Route path='/kontakt' component={Contact}></Route>
           <Route path='/register' component={Register}></Route>
+          <Route path='/Psi' component={dog}></Route>
+          <Route path='/Mačke' component={cat}></Route>
+          <Route path='/Ptice' component={bird}></Route>
+          <Route path='/Zečevi' component={rabbit}></Route>
+          <Route path='/Ribe' component={fish}></Route>
+          <Route path='/Glodari' component={rodent}></Route>
+          <Route path='/Reptili' component={reptil}></Route>
+          <Route path='/Domaće_Životinje' component={domesticAnimal}></Route>
+          <Route path='/Zglavkari' component={arthropod}></Route>
         </Switch>
       </Router>
       
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,11 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Category from '../components/Category';
-import dog from '../pages/Dog';
-import cat from '../pages/Cat';
-import bird from '../pages/Bird';
-import rabbit from '../pages/Rabbit';
-import fish from '../pages/Fish';
-import rodent from '../pages/Rodent';
-import reptil from '../pages/Reptil';
-import domesticAnimal from '../pages/DomesticAnimal';
-import arthropod from '../pages/Arthropod';
+
 
 function home() {
   return (
     <>
     <Category />
-    <Router>
-      <Switch>
-        <Route path='/Psi' component={dog}></Route>
-        <Route path='/Mačke' component={cat}></Route>
-        <Route path='/Ptice' component={bird}></Route>
-        <Route path='/Zečevi' component={rabbit}></Route>
-        <Route path='/Ribe' component={fish}></Route>
-        <Route path='/Glodari' component={rodent}></Route>
-        <Route path='/Reptili' component={reptil}></Route>
-        <Route path='/Domaće_Životinje' component={domesticAnimal}></Route>
-        <Route path='/Zglavkari' component={arthropod}></Route>
-      </Switch>
-    </Router>
-    
-    </>
+   </>
   );
 }
 
