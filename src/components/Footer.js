c6f31df08167cf354889dc7aa36a571a7653b23c
Cadule <dusancvetkovic98@gmail.com>
2021-06-22 17:14:47 +0200

    Footer probni gotov

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,21 @@
+import '../css/Footer.css';
 
 function Footer() {
     return (
 
-        <footer>
-            <div class="copyright">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-md-6 client">
-                            <p>© 2021&nbsp;<strong>Flaki</strong>. Sva prava zadržana.&nbsp;</p>
-                        </div>
-                        <div class="col-md-6 power">
-                            <p>Izrada sajta: Marko, Janko, Dusan</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+<footer class="footer">
+<div class="container">
+<ul class="foote_bottom_ul_amrc">
+<li><a href="#">Home</a></li>
+<li><a href="#">About</a></li>
+<li><a href="#">Services</a></li>
+<li><a href="#">Pricing</a></li>
+<li><a href="#">Contact</a></li>
+</ul>
+<p class="text-center">Copyright @2021 | Designed With by <a href="#">Marko Janko Dusan</a></p>
+</div>
+
+</footer>
     );
   }
   
