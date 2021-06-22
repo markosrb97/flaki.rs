62d91e36ac3c84dde12f25eec2bea7b6079a4d97
Marko Radosavljevic <50467436+markosrb97@users.noreply.github.com>
2021-06-22 14:16:08 +0200

    test

--- src/components/Recommended-banner.js
+++ src/components/Recommended-banner.js
@@ -1,50 +1,70 @@
 import React, { Component } from "react";
 import Slider from "react-slick";
-import '../css/Recommended-baner.css';
-import image from "../images/logo.png"
+import Image from "../images/logo.png"
 
-export default class SimpleSlider extends Component {
+export default class Responsive extends Component {
   render() {
-    const settings = {
+    var settings = {
       dots: false,
-      infinite: true,
+      infinite: false,
       speed: 500,
       slidesToShow: 4,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
     return (
       <div>
-        <h2>Istaknute životinje</h2>
+        <h2> Responsive </h2>
         <Slider {...settings}>
           <div>
-            <div>
-                <img src={image} alt="Slider-image" />
-            </div>
+            <img src={Image}></img>
           </div>
           <div>
-            <div>
-                <img src={image} alt="Slider-image" />
-            </div>
+          <img src={Image}></img>
           </div>
           <div>
-            <div>
-                <img src={image} alt="Slider-image" />
-            </div>
+          <img src={Image}></img>
           </div>
           <div>
-            <div>
-                <img src={image} alt="Slider-image" />
-            </div>
+          <img src={Image}></img>
           </div>
           <div>
-            <div>
-                <img src={image} alt="Slider-image" />
-            </div> 
+          <img src={Image}></img>
           </div>
           <div>
-            <div>
-                <img src={image} alt="Slider-image" />
-            </div> 
+          <img src={Image}></img>
+          </div>
+          <div>
+          <img src={Image}></img>
+          </div>
+          <div>
+          <img src={Image}></img>
           </div>
         </Slider>
       </div>
