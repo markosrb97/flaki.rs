dd5b156d11c733390962e43a925939e55514c925
Marko Radosavljevic <50467436+markosrb97@users.noreply.github.com>
2021-06-23 15:35:31 +0200

    izmenice

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,13 @@
 import Category from '../components/Category';
-
+import Carousel from '../components/Carousel'
+import RecommendedBaner from '../components/Recommended-banner'
 
 function home() {
   return (
     <>
+    <Carousel />
     <Category />
+    <RecommendedBaner />
    </>
   );
 }
