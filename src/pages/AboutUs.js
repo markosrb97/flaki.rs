a2dc03d73478bc63b59cb0a4096f70c35e803099
Marko Radosavljevic <50467436+markosrb97@users.noreply.github.com>
2021-06-27 17:13:11 +0200

    push

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,7 +1,19 @@
+import image from "../images/about-us.jpg"
+
 function AboutUs() {
     return (
       <>
-        <h2>About Us Page</h2>
+        <div className="d-flex">
+          <div className="d-flex-50">
+            <img src={image} className="w-100" alt="About-us-photo"></img>
+          </div>
+          <div className="d-flex-50">
+            <h1> Naslov</h1>
+            <p>asddklasdnasjkdnaskdnasjkdnaskndasjndansdkasndkasndnaskdnasjkdnaskdnaskdnaskdnkasdnkasdn
+              asjkdnaskndkasjdnjaskdnjaskdnasjkndaskjdnasjkndasj</p>
+            <button>Dugme koje moze da sluzi a ne mora</button>
+          </div>
+        </div>
       </>
     );
   }
